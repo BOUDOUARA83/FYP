6bb2021f69c999a7d08b920b09c89b727f8555e2
Ethar <72096888+BOUDOUARA83@users.noreply.github.com>
2024-04-10 15:41:27 +0100

    Initial commit

---
+++ GoHalalApp/backend/db/Database.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const connectDatabase = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch (error) {
+        console.error('MongoDB connection error:', error);
+        process.exit(1); // Exit process with failure
+    }
+};
+
+module.exports = connectDatabase;
